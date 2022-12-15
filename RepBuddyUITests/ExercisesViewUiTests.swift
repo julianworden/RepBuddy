dc5f540b501f863af2fe7934dde254fdf6384c11
Julian Worden <93101937+julianworden@users.noreply.github.com>
2022-12-14 20:01:26 -0800

    Start Writing UI Tests for iOS App
    Basic UI Tests for ExercisesView have been written. Also performed miscellaneous cleanup tasks, such as removing the iPadOS and macOS SDKs from the project and disabling landscape orientations.

--- RepBuddyUITests/ExercisesViewUiTests.swift
+++ RepBuddyUITests/ExercisesViewUiTests.swift
@@ -7,32 +7,33 @@
 
 import XCTest
 
-final class RepBuddyUITests: XCTestCase {
-    let app = XCUIApplication()
+final class ExercisesViewUiTests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
         continueAfterFailure = false
+
+        app = XCUIApplication()
+        app.launchArguments = ["testing"]
         app.launch()
     }
 
-    override func tearDownWithError() throws {
-        
-    }
+    override func tearDownWithError() throws { }
 
     func test_LaunchApp_ExercisesTabNavigationTitleExists() {
-        let navigationTitle = app.navigationBars[UITestingConstants.Exercises]
+        let navigationTitle = app.navigationBars["Exercises"]
         XCTAssertTrue(navigationTitle.exists)
     }
 
     func test_LaunchApp_TabViewExists() {
         let tabBar = app
-            .tabBars[UITestingConstants.TabBar]
+            .tabBars["Tab Bar"]
         let exercisesTabButton = app
-            .tabBars[UITestingConstants.TabBar]
-            .buttons[UITestingConstants.Exercises]
+            .tabBars["Tab Bar"]
+            .buttons["Exercises"]
         let workoutsTabButton = app
-            .tabBars[UITestingConstants.TabBar]
-            .buttons[UITestingConstants.Workouts]
+            .tabBars["Tab Bar"]
+            .buttons["Workouts"]
 
         XCTAssertTrue(tabBar.exists)
         XCTAssertTrue(exercisesTabButton.exists)
@@ -41,9 +42,39 @@ final class RepBuddyUITests: XCTestCase {
 
     func test_LaunchApp_ExercisesTabIsSelected() {
         let exercisesTabButton = app
-            .tabBars[UITestingConstants.TabBar]
-            .buttons[UITestingConstants.Exercises]
+            .tabBars["Tab Bar"]
+            .buttons["Exercises"]
 
         XCTAssertTrue(exercisesTabButton.isSelected)
+    }
+
+    func test_LaunchApp_PlusNavigationBarButtonExists() {
+        let plusNavigationBarButton = app
+            .navigationBars["Exercises"]
+            .buttons["Add"]
+
+        XCTAssertTrue(plusNavigationBarButton.exists)
+    }
+
+    func test_WhenNoExercisesExists_TextIsShown() {
+        let noDataFoundText = app
+            .staticTexts["You haven't created any exercises. Use the plus button to create one!"]
+
+        XCTAssertTrue(noDataFoundText.exists)
+    }
+
+    func test_ExerciseAdded_RowDisplays() {
+        app.navigationBars["Exercises"].buttons["Add"].tap()
+
+        app.collectionViews.textFields["Name (required)"].tap()
+
+        app.keys["T"].tap()
+        app.keys["e"].tap()
+        app.keys["s"].tap()
+        app.keys["t"].tap()
+        app.buttons["Save Exercise"].tap()
+
+        XCTAssertEqual(app.collectionViews.activityIndicators.count, 1, "One exercise should be shown in ExercisesView.")
+        XCTAssertTrue(app.collectionViews.staticTexts["Test"].exists, "An exercise named 'test' should be displayed in ExercisesView")
     }
 }
